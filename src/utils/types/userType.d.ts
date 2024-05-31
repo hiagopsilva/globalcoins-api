03874224f997709f5c04397186b5cdc7f2bdaf73
Hiago Prates <hiagohps360@gmail.com>
2024-05-31 13:15:32 -0300

    feat: user type

---
+++ src/utils/types/userType.d.ts
@@ -0,0 +1,17 @@
+declare namespace UserType {
+  export type payload = {
+    name: string
+    lastName: string
+    email: string
+    password: string
+    confirmPassword: string
+  }
+
+  export type item = {
+    id: number
+    name: string
+    lastName: string
+    email: string
+    password: string
+  }
+}
